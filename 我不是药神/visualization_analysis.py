fff1d74b9e41b8df2f66d598886569880b38356e
Phenobarbital <254712190@qq.com>
2018-07-31 11:42:16 +0800

    Update visualization_analysis.py
    修改了一下score_draw函数,采用直接创建对应的数据框，然后从表格中读取相应的值，而不用一遍遍的遍历，最后精简了一下画图的程序

--- 我不是药神/visualization_analysis.py
+++ 我不是药神/visualization_analysis.py
@@ -169,88 +169,41 @@ def draw_citys_pic(csv_file):
 
 def score_draw(csv_file):
     page = Page(csv_file+":评论等级分析")
-    score, date, val, score_list = [], [], [], []
-    result = {}
     path = os.path.abspath(os.curdir)
     csv_file = path + "\\" + csv_file + ".csv"
     csv_file = csv_file.replace('\\', '\\\\')
-    d = pd.read_csv(csv_file, engine='python', encoding='utf-8')[['score', 'date']].dropna()  # 读取CSV转为dataframe格式，并丢弃评论为空的记录
-    for indexs in d.index:  # 一种遍历df行的方法（下面还有第二种，iterrows）
-        score_list.append(tuple(d.loc[indexs].values[:])) # 目前只找到转换为tuple然后统计相同元素个数的方法
-    #print("有效评分总数量为：",len(score_list), " 条")
-    for i in set(list(score_list)):
-        result[i] = score_list.count(i)  # dict类型 ('很差', '2018-04-28'): 55
-    info = []
-    for key in result:
-        score= key[0]
-        date = key[1]
-        val = result[key]
-        info.append([score, date, val])
-    info_new = DataFrame(info)  # 将字典转换成为数据框
-    info_new.columns = ['score', 'date', 'votes']
-    info_new.sort_values('date', inplace=True)    # 按日期升序排列df，便于找最早date和最晚data，方便后面插值
-    #print("first df", info_new)
-    # 以下代码用于插入空缺的数据，每个日期的评分类型应该有5中，依次遍历判断是否存在，若不存在则往新的df中插入新数值
-    mark = 0
-    creat_df = pd.DataFrame(columns = ['score', 'date', 'votes']) # 创建空的dataframe
-    for i in list(info_new['date']):
-        location = info_new[(info_new.date==i)&(info_new.score=="力荐")].index.tolist()
-        if location == []:
-            creat_df.loc[mark] = ["力荐", i, 0]
-            mark += 1
-        location = info_new[(info_new.date==i)&(info_new.score=="推荐")].index.tolist()
-        if location == []:
-            creat_df.loc[mark] = ["推荐", i, 0]
-            mark += 1
-        location = info_new[(info_new.date==i)&(info_new.score=="还行")].index.tolist()
-        if location == []:
-            creat_df.loc[mark] = ["还行", i, 0]
-            mark += 1
-        location = info_new[(info_new.date==i)&(info_new.score=="较差")].index.tolist()
-        if location == []:
-            creat_df.loc[mark] = ["较差", i, 0]
-            mark += 1
-        location = info_new[(info_new.date==i)&(info_new.score=="很差")].index.tolist()
-        if location == []:
-            creat_df.loc[mark] = ["很差", i, 0]
-            mark += 1
-    info_new = info_new.append(creat_df.drop_duplicates(), ignore_index=True)
-    score_list = []
-    info_new.sort_values('date', inplace=True)    # 按日期升序排列df，便于找最早date和最晚data，方便后面插值
-    #print(info_new)
-    for index, row in info_new.iterrows():   # 第二种遍历df的方法
-        score_list.append([row['date'], row['votes'], row['score']])
-    tr = ThemeRiver()
-    tr.add(['力荐', '推荐', '还行', '较差', '很差'], score_list, is_label_show=True, is_more_utils=True)
-    page.add_chart(tr)
-
-    attr, v1, v2, v3, v4, v5 = [], [], [], [], [], []
-    attr = list(sorted(set(info_new['date'])))
-    bar = Bar()
-    for i in attr:
-        v1.append(int(info_new[(info_new['date']==i)&(info_new['score']=="力荐")]['votes']))
-        v2.append(int(info_new[(info_new['date']==i)&(info_new['score']=="推荐")]['votes']))
-        v3.append(int(info_new[(info_new['date']==i)&(info_new['score']=="还行")]['votes']))
-        v4.append(int(info_new[(info_new['date']==i)&(info_new['score']=="较差")]['votes']))
-        v5.append(int(info_new[(info_new['date']==i)&(info_new['score']=="很差")]['votes']))
-    bar.add("力荐", attr, v1, is_stack=True)
-    bar.add("推荐", attr, v2, is_stack=True)
-    bar.add("还行", attr, v3, is_stack=True)
-    bar.add("较差", attr, v4, is_stack=True)
-    bar.add("很差", attr, v5, is_stack=True, is_convert=True, mark_line=["average"], is_more_utils=True)
-    page.add_chart(bar)
-
-    line = Line()
-    line.add("力荐", attr, v1, is_stack=True)
-    line.add("推荐", attr, v2, is_stack=True)
-    line.add("还行", attr, v3, is_stack=True)
-    line.add("较差", attr, v4, is_stack=True)
-    line.add("很差", attr, v5, is_stack=True, is_convert=False, mark_line=["average"], is_more_utils=True)
-    page.add_chart(line)
-
+    d = pd.read_csv(csv_file, engine='python', encoding='utf-8')[['score', 'date']].dropna()  # 读取CSV转为dataframe格式，并丢弃评论为空的的记录
+    
+    
+    time = sorted(set(d['date'].dropna())) #先把所有的评论时间提取出来去重，排序
+    score_data = pd.DataFrame(np.zeros((len(time),5)),columns=['力荐','还行','推荐','较差','很差'],index=time)  #创建一个表格，其索引是时间，列是score，然后值为评论的数量，用0填充
+    def count_score(score_level): #score_level 即score的等级：还行，力荐等等
+        for i in d[d['score']==score_level]['date'].value_counts().index: #遍历score——level下的时间，然后提取出相应的数量，保存在score_data中
+            score_data.loc[i][score_level] = d[d['score']==score_level]['date'].value_counts()[i] 
+    
+    for score_level in score_data.columns: #填充数据，没有的自然就为0
+        count_score(score_level)
+        
+    score_bar = pyecharts.Bar('观影人数评分柱状图')
+    for score_level in score_data.columns:
+        score_bar.add('{}'.format(score_level),score_data.index,score_data[score_level],is_stack = True,is_convert=True)
+    page.add_chart(scor_bar)
+    
+    score_line = pyecharts.Line('观影人数评分折线图')
+    for score_level in score_data.columns:
+        score_line.add('{}'.format(score_level),score_data.index,score_data[score_level],is_stack = True,xaxis_rotate = 45)
+    page.add_chart(scor_line)
+    
+    score_river_data = [] #河流主题图的数据
+    for i in range(len(time)):
+        for j in range(5):
+            score_river_data.append([score_data.index[i],score_data.iloc[i,j],score_data.columns[j]])
+    score_theme_river = pyecharts.ThemeRiver("主题河流示意图")
+    score_theme_river.add(score_data.columns,score_river_data,is_label_show = True)
+    page.add_chart(score_theme_river)
     page.render(csv_file[:-4] + "_日投票量分析汇总.html")
 
-
+    
 def main(csv_file, stopwords_path, pic_path):
     # draw_sentiment_pic(csv_file)
     # draw_citys_pic(csv_file)
